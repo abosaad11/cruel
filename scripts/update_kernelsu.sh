89f17211c006d28413e177f84a8574d97adf6300
DJLeleX <29524391+DJLeleX@users.noreply.github.com>
2025-06-23 09:27:44 +0200

    Update update_kernelsu.sh
    Switch from next-susfs branch to next branch, as susfs branch is gone.

--- scripts/update_kernelsu.sh
+++ scripts/update_kernelsu.sh
@@ -7,10 +7,10 @@ flock -n 9 || exit 0
 AUTHOR="KernelSU-Next"
 REPO="KernelSU-Next"
 #LATEST_RELEASE=$(curl -s -k "https://api.github.com/repos/KernelSU-Next/KernelSU-Next/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
-#VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1&sha=$LATEST_RELEASE" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
+VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1&sha=$LATEST_RELEASE" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 
 # Latest version fetched from next-susfs branch is always two commit newer than the latest release
-VERSION=`curl -s -I -k "https://api.github.com/repos/KernelSU-Next/KernelSU-Next/commits?per_page=1&sha=next-susfs" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
+#VERSION=`curl -s -I -k "https://api.github.com/repos/KernelSU-Next/KernelSU-Next/commits?per_page=1&sha=next-susfs" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 
 if [[ -f drivers/kernelsu/.version && *$(cat drivers/kernelsu/.version)* == *$VERSION* ]]; then
 	touch drivers/kernelsu/.check
@@ -22,10 +22,10 @@ rm -rf drivers/kernelsu
 mkdir -p drivers/kernelsu
 cd drivers/kernelsu
 # This to get latest version without susfs
-# wget -q -O - "https://github.com/$AUTHOR/$REPO/archive/refs/tags/$LATEST_RELEASE.tar.gz" | tar -xz --strip=2 "$REPO-${LATEST_RELEASE#v}/kernel"
+wget -q -O - "https://github.com/$AUTHOR/$REPO/archive/refs/tags/$LATEST_RELEASE.tar.gz" | tar -xz --strip=2 "$REPO-${LATEST_RELEASE#v}/kernel"
 
-# This to get latest version with susfs
-wget -q -O - "https://github.com/$AUTHOR/$REPO/archive/refs/heads/next-susfs.tar.gz" | tar -xz --strip=2 "$REPO-next-susfs/kernel"
+# This to get latest version with susfs (susfs branch is gone?)
+# wget -q -O - "https://github.com/$AUTHOR/$REPO/archive/refs/heads/next-susfs.tar.gz" | tar -xz --strip=2 "$REPO-next-susfs/kernel"
 echo $VERSION >> .version
 touch .check
 
@@ -33,5 +33,5 @@ touch .check
 echo "" >> Makefile
 sed -i '/warning /d' Makefile
 sed -i '/DKSU_VERSION/d' Makefile
-#echo "ccflags-y += -DKSU_VERSION=$((10000 + $VERSION + 200))" >> Makefile
-echo "ccflags-y += -DKSU_VERSION=$((10000 + $VERSION + 198))" >> Makefile
+echo "ccflags-y += -DKSU_VERSION=$((10000 + $VERSION + 200))" >> Makefile
+#echo "ccflags-y += -DKSU_VERSION=$((10000 + $VERSION + 198))" >> Makefile
