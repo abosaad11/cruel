e6a5efdaef01560c36c61c456d1776ef5d89c362
DJLeleX <29524391+DJLeleX@users.noreply.github.com>
2025-07-08 14:11:07 +0200

    Adapt to sidex15's KernelSU-Next fork

--- scripts/update_kernelsu.sh
+++ scripts/update_kernelsu.sh
@@ -4,13 +4,13 @@ exec 9>.kernelsu-fetch-lock
 flock -n 9 || exit 0
 [[ $(( $(date +%s) - $(stat -c %Y "drivers/kernelsu/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-AUTHOR="KernelSU-Next"
+AUTHOR="sidex15"
 REPO="KernelSU-Next"
-#LATEST_RELEASE=$(curl -s -k "https://api.github.com/repos/KernelSU-Next/KernelSU-Next/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
+#LATEST_RELEASE=$(curl -s -k "https://api.github.com/repos/$AUTHOR/$REPO/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
 #VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1&sha=$LATEST_RELEASE" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 
 # Latest version fetched from next-susfs branch is always two commit newer than the latest release
-VERSION=`curl -s -I -k "https://api.github.com/repos/KernelSU-Next/KernelSU-Next/commits?per_page=1&sha=next-susfs" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
+VERSION=`curl -s -I -k "https://api.github.com/repos/$AUTHOR/$REPO/commits?per_page=1&sha=next-susfs" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p'`
 
 if [[ -f drivers/kernelsu/.version && *$(cat drivers/kernelsu/.version)* == *$VERSION* ]]; then
 	touch drivers/kernelsu/.check
